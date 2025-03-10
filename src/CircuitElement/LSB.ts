e78dc66bcfbd1a8ce184cca54a368c5a5fd1d2a2
Austin Hargis <hargis.austin@protonmail.com>
2025-03-10 11:54:28 -0400

    Fixed LSB circuit

--- src/CircuitElement/LSB.ts
+++ src/CircuitElement/LSB.ts
@@ -1,14 +1,31 @@
 import { CircuitElement } from "../CircuitElement";
+import { CircuitBus } from "../CircuitBus";
+import { BitString } from "../BitString";
 
 export class LSB extends CircuitElement {
-  #bitstring: string;
+  #bitstring: BitString;
 
-  constructor(bitstring: string) {
-    super();
-    this.#bitstring = bitstring;
+  constructor(inputs: CircuitBus[], outputs: CircuitBus[]) {
+    super("LSB", inputs, outputs);
+    this.#bitstring = this.getInputs()[0].getValue();
   }
 
   resolve(): number {
-    return parseInt(this.#bitstring, 2) & 1;
+    const inputs = this.getInputs();
+    const outputs = this.getOutputs();
+
+    this.#bitstring = inputs[0].getValue();
+
+    // Perform bitwise AND to find LSB
+    const lsb = Number(this.#bitstring) & 1;
+
+    outputs[0].setValue(new BitString(String(lsb)));
+
+    // Set the ENABLE output if it exists
+    if (outputs.length > 1) {
+      outputs[1].setValue(new BitString(String(1)));
+    }
+
+    return 10;
   }
 }
