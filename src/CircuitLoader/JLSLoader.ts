7c1eefaab0b6cbb717903b20382115c274da2b0d
Austin Hargis <hargis.austin@protonmail.com>
2025-03-28 07:58:31 -0400

    Pull apart unzipped JLSCircuit file to find elements

--- src/CircuitLoader/JLSLoader.ts
+++ src/CircuitLoader/JLSLoader.ts
@@ -1,12 +1,68 @@
 import { CircuitLoader } from "../CircuitLoader";
 import { CircuitProject } from "../CircuitProject";
+import { LogLevel } from "../CircuitLogger";
+import { CircuitElement } from "../CircuitElement";
+import { AndGate } from "../CircuitElement/AndGate";
+import { CircuitBus } from "../CircuitBus";
+import { XorGate } from "../CircuitElement/XorGate";
+
+type CircuitContext = {
+  nodes: CircuitBus[];
+  data: any;
+  project: CircuitProject;
+};
+
+const createElement: Record<string, (ctx: CircuitContext) => CircuitElement> = {
+  AndGate: ({ nodes, data }) =>
+    new AndGate(
+      data.customData.nodes.inp.map((i: number) => nodes[i]),
+      [nodes[data.customData.nodes.output1]],
+    ),
+  XorGate: ({ nodes, data }) =>
+    new XorGate(
+      data.customData.nodes.inp.map((i: number) => nodes[i]),
+      [nodes[data.customData.nodes.output1]],
+    ),
+};
 
 export class JLSCircuitLoader extends CircuitLoader {
   constructor() {
     super("JLSCircuitLoader");
   }
 
+  /*
+      TODO: This current solution requires the user to
+        unzip a .jls file and extract the JLSCircuit from inside.
+        *Ideal* end goal would be to handle the unzipping internally for
+        the purposes of testing
+     */
   load(data: any): CircuitProject {
-    throw new Error("Method not implemented.");
+    const project: CircuitProject = new CircuitProject();
+    this.propagateLoggersTo(project);
+
+    this.log(LogLevel.INFO, `Loading circuit from data ${data}`);
+
+    // Get all circuit parts
+    const lines: string[] = data.split("\n");
+
+    // Blacklist elements without nodes or that are visual only
+    const blacklistKeys = ["WireEnd"];
+
+    let elementsList: string[] = [];
+    for (let line of lines) {
+      line = line.replace(/[\n\r]+/g, "");
+
+      // Identify new element
+      if (line.startsWith("ELEMENT")) {
+        const lineParts = line.split(" ");
+        if (!blacklistKeys.includes(lineParts[1])) {
+          elementsList.push(lineParts[1]);
+        }
+      }
+    }
+
+    console.log(elementsList);
+
+    return project;
   }
 }
---
+++ tests/jls/jls.test.ts
@@ -0,0 +1,12 @@
+import { beforeAll } from "@jest/globals";
+import { Circuit, loadProject } from "../../src";
+import { JLSCircuitLoader } from "../../src/CircuitLoader/JLSLoader";
+
+let bitSelector: Circuit;
+
+beforeAll(async () => {
+  const project = await loadProject(JLSCircuitLoader, "tests/jls/JLSCircuit");
+  // bitSelector = project.getCircuitByName("BitSelector");
+});
+
+test("Sample", async () => {});
