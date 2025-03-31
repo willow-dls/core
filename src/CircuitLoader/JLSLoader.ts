df40382ccf2437cc022a957182d5d014d9583bc2
Austin Hargis <hargis.austin@protonmail.com>
2025-03-30 21:31:09 -0400

    Began building out actual circuit generation

--- src/CircuitLoader/JLSLoader.ts
+++ src/CircuitLoader/JLSLoader.ts
@@ -10,6 +10,10 @@ import { Output } from "../CircuitElement/Output";
 import { SubCircuit } from "../CircuitElement/SubCircuit";
 import Stream from "stream";
 import { FileUtil } from "../Util/File";
+import { NotGate } from "../CircuitElement/NotGate";
+import { OrGate } from "../CircuitElement/OrGate";
+import { Splitter } from "../CircuitElement/Splitter";
+import { Circuit } from "../Circuit";
 
 type CircuitContext = {
   nodes: CircuitBus[];
@@ -38,6 +42,24 @@ const createElement: Record<string, (ctx: CircuitContext) => CircuitElement> = {
       data.inputNodes.map((nodeInd: number) => nodes[nodeInd]),
       data.outputNodes.map((nodeInd: number) => nodes[nodeInd]),
     ),
+  NotGate: ({ nodes, data }) =>
+    new NotGate(
+      data.customData.nodes.inp.map((i: number) => nodes[i]),
+      [nodes[data.customData.nodes.output1]],
+    ),
+  OrGate: ({ nodes, data }) =>
+    new OrGate(
+      data.customData.nodes.inp.map((i: number) => nodes[i]),
+      [nodes[data.customData.nodes.output1]],
+    ),
+  Splitter: ({ nodes, data }) =>
+    new Splitter(
+      // No, this is not a typo in our code, their data file actually has "constructorParamaters"
+      // instead of the proper spelling "constructorParameters"...
+      data.customData.constructorParamaters[2],
+      nodes[data.customData.nodes.inp1],
+      data.customData.nodes.outputs.map((nodeInd: number) => nodes[nodeInd]),
+    ),
 };
 
 export class JLSCircuitLoader extends CircuitLoader {
@@ -49,8 +71,9 @@ export class JLSCircuitLoader extends CircuitLoader {
     const project: CircuitProject = new CircuitProject();
     this.propagateLoggersTo(project);
 
-    const data = await FileUtil.extractFromZip(stream, ['JLSCircuit'])
-      .then(([stream]) => FileUtil.readTextStream(stream));
+    const data = await FileUtil.extractFromZip(stream, ["JLSCircuit"]).then(
+      ([stream]) => FileUtil.readTextStream(stream),
+    );
 
     this.log(LogLevel.INFO, `Loading circuit from data ${data}`);
 
@@ -58,11 +81,12 @@ export class JLSCircuitLoader extends CircuitLoader {
     const lines: string[] = data.split("\n");
 
     // Blacklist elements without nodes or that are visual only
-    const blacklistKeys = ["WireEnd"];
+    const blacklistKeys = ["WireEnd", "Text"];
 
     let circuitStack: string[] = [];
     let circuitMap: Record<string, any> = {};
 
+    // Generate a list of all of the different types of circuits
     for (let line of lines) {
       const lineParts = line.replace(/[\n\r]+/g, "").split(" ");
 
@@ -81,7 +105,39 @@ export class JLSCircuitLoader extends CircuitLoader {
       }
     }
 
-    console.log(circuitStack);
+    let arbitraryIdx: number = 0;
+    // Iterate for every circuit
+    for (const circuitName of Object.keys(circuitMap)) {
+      const circuitElements: CircuitElement[] = [];
+
+      // Iterate for every element in the circuit map
+      for (const elementType of circuitMap[circuitName]) {
+        // Establish data for element
+        const data = {
+          customData: {
+            nodes: { output1: 0 },
+            values: [],
+          },
+          index: arbitraryIdx,
+        };
+
+        // TODO: Build nodes and data
+        const newElement = createElement[elementType]({
+          project: project,
+          nodes: [],
+          data: data,
+        });
+        circuitElements.push(newElement);
+      }
+
+      const circuit = new Circuit(
+        arbitraryIdx.toString(),
+        circuitName,
+        circuitElements,
+      );
+      project.addCircuit(circuit);
+    }
+
     console.log(circuitMap);
 
     return project;
