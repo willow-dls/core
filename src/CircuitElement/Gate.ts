64b122212a8272e5d7cc527bf7f500c76f4a0639
AaronMacD <aaron.j.macd@gmail.com>
2025-04-17 13:59:37 -0400

    Changed line 23 from inputValues.length == 1 to inputValues[0] != null

--- src/CircuitElement/Gate.ts
+++ src/CircuitElement/Gate.ts
@@ -3,40 +3,42 @@ import { CircuitBus } from "../CircuitBus";
 import { BitString } from "../BitString";
 
 export abstract class Gate extends CircuitElement {
-  constructor(inputs: CircuitBus[], outputs: CircuitBus[]) {
-    super("GateElement", inputs, outputs);
-  }
+    constructor(inputs: CircuitBus[], outputs: CircuitBus[]) {
+        super("GateElement", inputs, outputs);
+    }
 
-  resolve(): number {
-    const inputs = this.getInputs();
-    const outputs = this.getOutputs();
+    resolve(): number {
+        const inputs = this.getInputs();
+        const outputs = this.getOutputs();
 
-    // // If there are null inputs, we can't evaluate this circuit
-    // // in any meaningful way.
-    // if (inputs.filter(i => i.getValue() == null).length > 0) {
-    //     outputs.forEach(o => o.setValue(BitString.low()));
-    //     return this.getPropagationDelay();
-    // }
+        // // If there are null inputs, we can't evaluate this circuit
+        // // in any meaningful way.
+        // if (inputs.filter(i => i.getValue() == null).length > 0) {
+        //     outputs.forEach(o => o.setValue(BitString.low()));
+        //     return this.getPropagationDelay();
+        // }
 
-    const inputValues = inputs.map((i) => i.getValue());
-    const result: BitString | null =
-      inputValues.length == 1
-        ? this.evaluate(BitString.low(), inputValues[0] as BitString)
-        : inputValues
-            .slice(1)
-            .reduce(
-              (prev, cur) =>
-                this.evaluate(prev ?? BitString.low(), cur ?? BitString.low()),
-              inputValues[0],
-            );
+        const inputValues = inputs.map((i) => i.getValue());
+        const result: BitString | null =
+            inputValues[0] != null
+                ? this.evaluate(BitString.low(), inputValues[0] as BitString)
+                : inputValues
+                    .slice(1)
+                    .reduce(
+                        (prev, cur) => {
 
-    outputs.forEach((o) => o.setValue(result));
+                            return this.evaluate(prev ?? BitString.low(), cur ?? BitString.low())
+                        },
+                        inputValues[0],
+                    );
 
-    return this.getPropagationDelay();
-  }
+        outputs.forEach((o) => o.setValue(result));
 
-  abstract evaluate(
-    previousValue: BitString,
-    currentValue: BitString,
-  ): BitString;
+        return this.getPropagationDelay();
+    }
+
+    abstract evaluate(
+        previousValue: BitString,
+        currentValue: BitString,
+    ): BitString;
 }
